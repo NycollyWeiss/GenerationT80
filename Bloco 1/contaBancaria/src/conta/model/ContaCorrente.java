e47e846497dd9dc6de7b050ec7e9850ffd0bb2fc
NycollyWeiss <nycollyweissoliveira@gmail.com>
2025-02-14 19:43:42 -0300

    Criacao e Implementacao das Classes Model ContaCorrente e ContaPoupanca e Interface ContaRepository

---
+++ Bloco 1/contaBancaria/src/conta/model/ContaCorrente.java
@@ -0,0 +1,44 @@
+package conta.model;
+
+public class ContaCorrente extends Conta {
+
+	
+	private double limite;
+	
+	public ContaCorrente(int numero, int agencia, int tipo, String titular, double saldo, double limite) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.limite = limite;
+	}
+
+	public double getLimite() {
+		return limite;
+	}
+
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+
+	public boolean sacar (double valor) {
+		
+		if(this.getSaldo() + this.getLimite()< valor) {
+			System.out.println("Saldo Insuficiente!");
+			return false;
+		}
+		
+		else {
+			this.setSaldo(this.getSaldo()-valor);
+			return true;
+		}
+		
+		
+			}
+	
+	 @Override
+		public void visualizar() {
+			super.visualizar();
+			System.out.println("Limite de Crédito: " + this.limite);
+	
+	}
+}
+
+
